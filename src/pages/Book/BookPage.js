e67b415cd8e90a050c4735ef065ba5dbf5a9ee92
Pete Walker <108812366+petertimwalker@users.noreply.github.com>
2024-05-25 11:30:06 -0400

    mv book logic into book dir

--- src/pages/Book/BookPage.js
+++ src/pages/Book/BookPage.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero";
-import Books from "../components/Books";
+import BookSearch from "BookSearch";
 
 function BookPage(props) {
         return (
                 <div className="App">
                         <Hero title={props.title} subTitle={props.subTitle} text={props.text} />
-                        <Books />
+                        <BookSearch />
                 </div>
         );
 }
